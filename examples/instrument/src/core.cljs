bf77a07e08faf609305fb5bf89f739b8001047d8
Robert Stuttaford <robert@cognician.com>
2014-03-13 13:49:41 +0200

    Woohoo.

--- examples/instrument/src/core.cljs
+++ examples/instrument/src/core.cljs
@@ -1,43 +1,103 @@
 (ns examples.instrument.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.reader :as reader]))
 
 (enable-console-print!)
 
-(defn sub-view [item owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div nil (:text item)))))
+;; =============================================================================
+;; Declarations
 
-(defn app-view [app owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div nil
-        (dom/div nil (:text app))
-        (apply dom/ul nil
-          (om/build-all sub-view (:list app)))))))
+(def app-state
+  (atom {:ui [{:checked false :label "Foo" :count 0}
+              {:checked false :label "Bar" :count 0}
+              {:checked false :label "Baz" :count 0}]}))
 
-(defn something-else [original owner opts]
+;; =============================================================================
+;; Application
+
+(defn radio-button [data owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div #js {:style #js {:border "1px solid #ccc"
-                                :padding "5px"}}
+      (dom/div #js {:className "radio"}
+        (dom/input #js {:type "checkbox"
+                        :checked (:checked data)
+                        :onChange
+                        (fn [e]
+                          (om/transact! data :checked not)
+                          (om/transact! data :count inc))})
+        (dom/label nil (:label data))))))
+
+(defn all-buttons [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/div nil
+        (om/build-all radio-button (:ui data))))))
+
+;; =============================================================================
+;; Inspection
+
+(defn pr-map-cursor [cursor]
+  (pr-str
+    (into cljs.core.PersistentHashMap.EMPTY
+      (om/value cursor))))
+
+(defn handle-change [e cursor owner]
+  (let [value (.. e -target -value)]
+    (try
+      (let [data (reader/read-string value)]
+        (if (= (set (keys @cursor)) (set (keys data)))
+          (do
+            (om/transact! cursor (fn [_] data))
+            (om/set-state! owner :value value))
+          (om/update-state! owner :value identity)))
+      (catch :default ex
+        (om/set-state! owner :value value)))))
+
+(defn pr-map-cursor [cursor]
+  (pr-str
+    (into cljs.core.PersistentHashMap.EMPTY
+      (om/value cursor))))
+
+(defn editor [[_ cursor :as original] owner opts]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:value (pr-map-cursor cursor)
+       :editing false})
+    om/IRenderState
+    (render-state [_ {:keys [editing value]}]
+      (dom/div #js {:className "editor"}
         (dom/div nil
-          (dom/span nil "Path:")
-          (dom/pre #js {:style #js {:display "inline-block"}}
-            (pr-str (om/path (second original)))))
+          (dom/label #js {:className "inspector"} "path:")
+          (dom/code nil (pr-str (om/path cursor))))
+        (dom/div nil
+          (dom/label #js {:className "inspector"} "value:")
+          (dom/input
+            #js {:className "edit"
+                 :value (if editing
+                          value
+                          (pr-map-cursor cursor))
+                 :onFocus (fn [e]
+                            (om/set-state! owner :editing true)
+                            (om/set-state! owner :value
+                              (pr-map-cursor (second (om/get-props owner)))))
+                 :onBlur (fn [e] (om/set-state! owner :editing false))
+                 :onChange #(handle-change % cursor owner)}))
         (apply om/build* original)))))
 
-(om/root
-  app-view
-  (atom {:text "Instrument!"
-         :list [{:text "Milk"} {:text "Cookies"} {:text "Apples"}]})
-  {:target (.getElementById js/document "app")
+;; =============================================================================
+;; Init
+
+(om/root all-buttons app-state
+  {:target (.getElementById js/document "ex0")})
+
+(om/root all-buttons app-state
+  {:target (.getElementById js/document "ex1")
    :instrument
    (fn [f cursor m]
-     (if (= f sub-view)
-       (om/build* something-else [f cursor m])
+     (if (= f radio-button)
+       (om/build* editor (om/graft [f cursor m] cursor))
        ::om/pass))})
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om "0.5.2"
+(defproject om-sandbox "0.5.2"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
@@ -15,105 +15,11 @@
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
-  :cljsbuild {
-    :builds [{:id "test"
-              :source-paths ["src" "test"]
-              :compiler {
-                :preamble ["react/react.min.js"]
-                :output-to "script/tests.simple.js"
-                :output-dir "script/out"
-                :source-map "script/tests.simple.js.map"
-                :output-wrapper false
-                :optimizations :simple}}
-             ;; examples
-             {:id "hello"
-              :source-paths ["src" "examples/hello/src"]
-              :compiler {
-                :output-to "examples/hello/main.js"
-                :output-dir "examples/hello/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "mouse"
-              :source-paths ["src" "examples/mouse/src"]
-              :compiler {
-                :output-to "examples/mouse/main.js"
-                :output-dir "examples/mouse/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "multiroot"
-              :source-paths ["src" "examples/multiroot/src"]
-              :compiler {
-                :output-to "examples/multiroot/main.js"
-                :output-dir "examples/multiroot/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "counters"
-              :source-paths ["src" "examples/counters/src"]
-              :compiler {
-                :output-to "examples/counters/main.js"
-                :output-dir "examples/counters/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "animation"
-              :source-paths ["src" "examples/animation/src"]
-              :compiler {
-                :output-to "examples/animation/main.js"
-                :output-dir "examples/animation/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "shared"
-              :source-paths ["src" "examples/shared/src"]
-              :compiler {
-                :output-to "examples/shared/main.js"
-                :output-dir "examples/shared/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "typeahead"
-              :source-paths ["src" "examples/typeahead/src"]
-              :compiler {
-                :output-to "examples/typeahead/main.js"
-                :output-dir "examples/typeahead/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "sortable"
-              :source-paths ["src" "examples/sortable/src"]
-              :compiler {
-                :output-to "examples/sortable/main.js"
-                :output-dir "examples/sortable/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "graft"
-              :source-paths ["src" "examples/graft/src"]
-              :compiler {
-                :output-to "examples/graft/main.js"
-                :output-dir "examples/graft/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "instrument"
-              :source-paths ["src" "examples/instrument/src"]
-              :compiler {
-                :output-to "examples/instrument/main.js"
-                :output-dir "examples/instrument/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "stateful"
-              :source-paths ["src" "examples/stateful/src"]
-              :compiler {
-                :output-to "examples/stateful/main.js"
-                :output-dir "examples/stateful/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "harmful"
-              :source-paths ["src" "examples/harmful/src"]
-              :compiler {
-                :output-to "examples/harmful/main.js"
-                :output-dir "examples/harmful/out"
-                :source-map true
-                :optimizations :none}}
-             {:id "tests"
-              :source-paths ["src" "examples/tests/src"]
-              :compiler {
-                :output-to "examples/tests/main.js"
-                :output-dir "examples/tests/out"
-                :source-map true
-                :optimizations :none}}]})
+  :cljsbuild
+  {:builds [{:id "instrument"
+             :source-paths ["src" "examples/instrument/src"]
+             :compiler
+             {:output-to "examples/instrument/main.js"
+              :output-dir "examples/instrument/out"
+              :source-map true
+              :optimizations :none}}]})
